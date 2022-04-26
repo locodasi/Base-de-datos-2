b311f175fd28d95328fa7f81051f57d0e8e023c6
Lucas   Da Silva <75490229+locodasi@users.noreply.github.com>
2022-04-26 08:38:34 -0300

    Update 01_Tablas e insercion inicial.sql

--- DDL/01_Tablas e insercion inicial.sql
+++ DDL/01_Tablas e insercion inicial.sql
@@ -22,15 +22,7 @@ constraint foreign key (proyecto) references Proyectos (proyecto)
 
 
 
-INSERT INTO rrhh() VALUES (1234,"Lucas"),(2222,"Marcos"),(3113,"Esther"),(1111,"Micaela"),(22,"Gladys"),(6677,"Juan"),
-(9980,"Moshe"),(1212,"Manuel"),(9999,"Nicolas"),(8787,"Susana");
 
 INSERT INTO proyectos() VALUES ("Apple","Red social","Cordoba 1222","Santa fe 23"),("Uruguay","Base de datos covid","Lavalle 345","Cordoba 2000"),
 ("Monster Inc","Maquina extractora de gritos","Panama 233","Viamonte 1200");
 
-INSERT INTO participantes () VALUES (1234, "Red social","Tester",40),(2222, "Red social","Tester",30),(3113, "Red social","Developer",40),
-(1111, "Base de datos covid","Tester",40),(22, "Base de datos covid","developer",60),(6677, "Base de datos covid","Diseñador",40),
-(9980,"Maquina extractora de gritos","Programador",25),(1212,"Maquina extractora de gritos","Programador",105),
-(9999,"Maquina extractora de gritos","Developer",98),(8787,"Maquina extractora de gritos","Marketing",65),
-(1111, "Red social","developer",60),(6677, "Red social","developer",60),(1234, "Base de datos covid","developer",60),
-(2222, "Base de datos covid","developer",60);
